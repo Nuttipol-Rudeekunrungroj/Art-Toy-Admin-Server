8076b88d37265567a3b2727ce90effb2c81d95db
nuttipol <nuttipol.r@gmail.com>
2024-11-27 16:45:39 +0700

    ProductSchema (models/Product.js)

---
+++ controllers/admin/products-controller.js
@@ -0,0 +1,37 @@
+const { imageUploadUtil } = require("../../helpers/cloudinary");
+
+
+
+const handleImageUpload = async (req, res) => {
+try{
+
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    const url = "data:" + req.file.mimetype + ";base64," + b64;
+    const result = await imageUploadUtil(url);
+    res.json({
+        success: true,
+        result,
+      });
+
+}catch(error){
+
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error occured",
+    });
+
+}
+
+}
+
+
+//add a new product
+
+//fetch all products
+
+//edit a product
+
+//delete a product
+
+module.exports = {handleImageUpload}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require ("cors")
 const authRouter= require("./routes/auth/auth-routes")
+const adminProductsRouter = require('./routes/admin/products-routes')
 
 mongoose
 .connect('mongodb+srv://trexblackhole:1WDZegPghdP0Vu4k@cluster0.eiyfe.mongodb.net/')
@@ -10,7 +11,7 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(
     cors({
@@ -31,6 +32,7 @@ app.use(
   app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api/admin/products",adminProductsRouter)
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
 
